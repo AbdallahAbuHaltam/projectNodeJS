0622947e750339c300dfcb344047f58dc890bcba
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-12-06 15:46:02 +0300

    Exporting Routes

---
+++ routes/tourRoutes.js
@@ -0,0 +1,92 @@
+const fs=require('fs');
+const express=require('express');
+
+const tours=JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
+
+const getAllTours=(req,res)=>{
+    console.log(req.requestTime)
+    res.status(200).json({
+        status: 'success',
+        requestedAt:req.requestTime,
+        results:tours.length,
+        data:{
+            tours
+        }
+        
+    });
+};
+const getTour=(req,res)=>{
+    const id=+req.params.id;
+    const tour=tours.find(el=>el.id===id);
+    if(id>tours.length){
+        return res.status(404).json({
+            status:'fail',
+            message:`Tour with ID ${id} not found.`
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        // results:tours.length,
+        // data:{
+        //     tours
+        // }
+        data:{
+            tour
+        }
+    });
+};
+const createNewTour=(req,res)=>{
+    const newID=tours[tours.length-1].id+1;
+    const newTour=Object.assign({
+        id:newID},
+        req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err=>{
+        res.status(201).json({
+            status: 'success',
+            results:tours.length,
+            data:{
+                tour:newTour
+            }
+        });
+    });
+};
+const updateTour=(req,res)=>{
+    const id=+req.params.id;
+    if(id>tours.length){
+        return res.status(404).json({
+            status:'fail',
+            message:`Tour with ID ${id} not found.`
+        });
+    }
+    res.status(200).json({
+        status:'success',
+        data:{
+            tour:'Updated tour'
+        }
+    });
+};
+const deleteTour=(req,res)=>{
+    const id=+req.params.id;
+    if(id>tours.length){
+        return res.status(404).json({
+            status:'fail',
+            message:`Tour with ID ${id} not found.`
+        });
+    }
+    res.status(200).json({
+        status:'success',
+        data:{
+            tour:'Updated tour'
+        }
+    });
+};
+
+
+const router=express.Router();
+
+router.route('/').get(getAllTours).post(createNewTour);
+router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+module.exports=router;
