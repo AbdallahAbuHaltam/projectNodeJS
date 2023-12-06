1187a0ef86a309e628eca009f86f9e2783b61ac4
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-12-06 20:37:19 +0300

    Serving Static files

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const userRouter=require('./routes/userRoutes');
 const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req,res,next)=>{
     console.log('Hellp from the Middleware');
     next();
