3cf5caa3a4356b6f46ae0fd0b35b178a8534997d
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-12-06 15:37:57 +0300

    Mount Routes

--- app.js
+++ app.js
@@ -129,19 +129,19 @@ const deleteUser=(req,res)=>{
 
 const tours=JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-
-// app.get('/api/v1/tours',getAllTours);
-// app.get('/api/v1/tours/:id',getTour);
-// app.post('/api/v1/tours',createNewTour);
-// app.patch('/api/v1/tours/:id',updateTour);
-// app.delete('/api/v1/tours/:id',deleteTour);
 //3 Route
-app.route('/api/v1/tours').get(getAllTours).post(createNewTour);
-app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour);
+const tourRouter=express.Router();
+const userRouter=express.Router();
 
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
-app.route('/api/v1/users/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
+tourRouter.route('/').get(getAllTours).post(createNewTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+
+app.use('/api/v1/tours',tourRouter);
+app.use('/api/v1/users',userRouter);
 
 //Server listen
 const port=3000;
