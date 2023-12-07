084de55fd8f14850b759b7f3ce449b74383a0fa6
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-12-07 21:24:55 +0300

    Reading Documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,19 +4,39 @@ const Tour=require("../models/tourModel");
 // const tours=JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
 
-exports.getAllTours=(req,res)=>{
-    console.log(req.requestTime)
+exports.getAllTours= async (req,res)=>{
+  try{
+    const tours=await Tour.find();
     res.status(200).json({
         status: 'success',
-        requestedAt:req.requestTime,
-        // results:tours.length,
-        // data:{
-        //     tours
-        // }
+        results:tours.length,
+        data:{
+            tours
+        }
         
     });
+  }catch(err){
+    res.status(404).json({
+        status:'fail',
+        message:err
+    });
+  }
 };
-exports.getTour=(req,res)=>{
+exports.getTour=async(req,res)=>{
+    try{
+        const tour=await Tour.findById(req.params.id);
+        res.status(200).json({
+            status:'succes',
+            data:{
+                tour
+            }
+        });
+    }catch(err){
+        res.status(404).json({
+            status:'fail',
+            message:err
+        });
+    }
     // const id=+req.params.id;
     // const tour=tours.find(el=>el.id===id);
     // res.status(200).json({
