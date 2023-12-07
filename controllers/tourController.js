59e25f5ff4b841e99d281ed45bc3edd96d8d69a5
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-12-07 13:12:26 +0300

    Creating Documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,15 +3,6 @@ const Tour=require("../models/tourModel");
 
 // const tours=JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
-exports.checkBody=(req,res,next)=>{
-    if(!req.body.name||!req.body.price){
-        return res.status(404).json({
-            status:'fail',
-            message:"Name and price are missing."
-        });
-    }
-    next();
-};
 
 exports.getAllTours=(req,res)=>{
     console.log(req.requestTime)
@@ -39,14 +30,25 @@ exports.getTour=(req,res)=>{
     //     }
     // });
 };
-exports.createNewTour=(req,res)=>{
+exports.createNewTour= async (req,res)=>{
+    try{
+    // const newTour= new Tour({});
+    // newTour.save();
+    const newTour=await Tour.create(req.body);
+
     res.status(201).json({
         status: 'success',
-        // results:tours.length,
-        // data:{
-        //     tour:newTour
-        // }
+            data:{
+             tour:newTour
+            }
     });
+    }catch(err){
+        res.status(400).json({
+            status:'fail',
+            message: 'INVALID DATA'
+        })
+    }
+   
     // const newID=tours[tours.length-1].id+1;
     // const newTour=Object.assign({
     //     id:newID},
