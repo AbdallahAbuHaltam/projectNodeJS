1246741943e5a2e5a4f890e1719e867b37684b28
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-12-10 15:07:51 +0300

    Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,7 +6,24 @@ const Tour=require("../models/tourModel");
 
 exports.getAllTours= async (req,res)=>{
   try{
-    const tours=await Tour.find();
+    //BUILD QUERY
+    // eslint-disable-next-line node/no-unsupported-features/es-syntax
+    const queryObj = {...req.query};
+    const excludedFields = ['page','sort','limit','fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+
+    const query = Tour.find(queryObj);
+    // const query =  Tour.find()
+    //     .where('duration')
+    //     .equals(5)
+    //     .where('difficulty')
+    //     .equals('easy');
+
+    //EXCUTE QUERY
+    const tours= await query;
+
+    //SEND RESPONSE
     res.status(200).json({
         status: 'success',
         results:tours.length,
