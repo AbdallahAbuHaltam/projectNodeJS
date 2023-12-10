f68248dbf5883ecf7127b0c0662ca78883abe9ed
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-12-10 13:56:45 +0300

    Deleting Documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -104,12 +104,19 @@ exports.updateTour=async (req,res)=>{
     }
     
 };
-exports.deleteTour=(req,res)=>{
-    res.status(200).json({
-        status:'success',
-        data:{
-            tour:'Updated tour'
-        }
-    });
+exports.deleteTour=async(req,res)=>{
+    try{
+        await Tour.findByIdAndDelete(req.params.id);
+        res.status(204).json({
+            status:'success',
+            data:null
+        });
+    }catch(err){
+        res.status(400).json({
+            status:'fail',
+            message: 'INVALID DATA'
+        });
+    }
+    
 };
 
