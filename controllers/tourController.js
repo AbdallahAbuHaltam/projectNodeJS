8402c5670ec9df065963f2313a04fda7ddb93b64
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-12-10 15:40:32 +0300

    Advanced Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -7,22 +7,26 @@ const Tour=require("../models/tourModel");
 exports.getAllTours= async (req,res)=>{
   try{
     //BUILD QUERY
+    //1)Filtering
     // eslint-disable-next-line node/no-unsupported-features/es-syntax
     const queryObj = {...req.query};
     const excludedFields = ['page','sort','limit','fields'];
     excludedFields.forEach(el => delete queryObj[el]);
 
+    //2)Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr=queryStr.replace(/\b(gte|gt|lte|lt)\b/g,match=>`$${match}`);
 
-    const query = Tour.find(queryObj);
+    
+    const query = Tour.find(JSON.parse(queryStr));
+    //EXCUTE QUERY
+    const tours= await query;
+
     // const query =  Tour.find()
     //     .where('duration')
     //     .equals(5)
     //     .where('difficulty')
     //     .equals('easy');
-
-    //EXCUTE QUERY
-    const tours= await query;
-
     //SEND RESPONSE
     res.status(200).json({
         status: 'success',
