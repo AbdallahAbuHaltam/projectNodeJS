5d020aeb9a1367b0da20347f6fb3a1afefdf4c34
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-12-29 19:31:15 +0300

    Implementing a Global Handle  Error

--- app.js
+++ app.js
@@ -7,12 +7,6 @@ const userRouter = require('./routes/userRoutes');
 //1 Middleware
 const app = express();
 
-app.all('*',(req,res,next)=>{
-  res.status(404).json({
-    status:'Fail',
-    message:`Can't Find ${req.originalUrl} on this server`
-  })
-});
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -34,5 +28,26 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 
+app.all('*',(req,res,next)=>{
+  // res.status(404).json({
+  //   status:'Fail',
+  //   message:`Can't Find ${req.originalUrl} on this server`
+  // });
+  const err = new Error(`Can't Find ${req.originalUrl} on this server`);
+  err.status='fail';
+  err.statusCode=404;
+
+  next(err);
+});
+
+app.use((err,req,res,next)=>{
+  err.statusCode=err.statusCode||500;
+  err.status=err.status||'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
